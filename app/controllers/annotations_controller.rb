0f86a1214c15fc417f17584a09d7c830e9d8aa3c
Alice <ays0110@gmail.com>
2017-07-06 16:18:05 -0400

    Remove bye bug whoops

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -3,7 +3,6 @@ class AnnotationsController < ApplicationController
   def create
     data = params[:data].blank? ? {} : params[:data]
     data[0][1]["d"] = data[0][1]["d"] + 'Z'
-    byebug
     image_id = params[:image_id]
     label = params[:label]
     @annotation = Annotation.create(:data=> data, :image_id=>image_id, :label=> label)
