594735792e26df17caf78c872656d67970e967ea
Alice Shen <ays0110@gmail.com>
2017-08-10 20:01:13 -0700

    Change main.m to handle coordinates for tiled analysis

--- algorithms/matlab/main.m
+++ algorithms/matlab/main.m
@@ -1,15 +1,15 @@
-function result = main(svs_file_path, extra_parameters_array, algorithm_name)
-
-
+function result = main(svs_file_path, extra_parameters_array, algorithm_name, tile_x, tile_y, tile_size)
    addpath('.')
-   svs_file = imread(svs_file_path,'Index',1);
+   Rows = [tile_x, tile_x + tile_size]
+   Cols = [tile_y, tile_y + tile_size]
+   roi = imread(svs_file_path,'Index',index,'PixelRegion',{Rows,Cols});
    extra_parameters_cell_array = num2cell(extra_parameters_array)
+
    preprocessing_function_handler = str2func(strcat(algorithm_name, '_preprocess_function'))
    main_function_handler = str2func(strcat(algorithm_name, '_main_function'))
    postprocessing_function_handler = str2func(strcat(algorithm_name, '_postprocess_function'))
    
-   main_input = preprocessing_function_handler(svs_file);
+   main_input = preprocessing_function_handler(roi);
    main_output = main_function_handler(main_input, extra_parameters_cell_array{:})
    postprocessing_function_handler(main_output)
-
 end
