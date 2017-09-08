90cd2d2567f8149651f376de9b6345e6ee94f7e8
Alice Shen <ays0110@gmail.com>
2017-09-08 16:46:53 -0400

    Remove early dotenv call

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
 
 module Imageviewer
   class Application < Rails::Application
