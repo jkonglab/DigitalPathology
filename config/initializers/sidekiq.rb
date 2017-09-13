ae2a6745e9dcdf1c73254789814b594f3003e027
Alice Shen <ays0110@gmail.com>
2017-09-13 17:32:23 -0400

    Redis on production

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+if environment == 'production'
+	Sidekiq.configure_server do |config|
+  		config.redis = { url: 'redis://192.168.1.7:6379' }
+	end
+
+	Sidekiq.configure_client do |config|
+  		config.redis = { url: 'redis://192.168.1.7:6379' }
+	end
+end
