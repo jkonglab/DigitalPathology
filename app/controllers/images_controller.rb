bdd29f6b484fe5b120a91d5cafe84df0c2c8b58d
Alice Shen <ays0110@gmail.com>
2017-09-13 17:21:19 -0400

    Use data directory in image upload

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -14,7 +14,7 @@ class ImagesController < ApplicationController
     original_filename = uploaded_io.original_filename #myfile.svs
     image = generate_new_image(original_filename)
 
-    File.open(Rails.root.join('public', 'data', image.upload_file_name), 'wb') do |file|
+    File.open(Rails.root.join('public', Rails.application.config.data_directory, image.upload_file_name), 'wb') do |file|
         file.write(uploaded_io.read)
     end
 
