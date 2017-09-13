46a783031fb6fe4ddf1d87dd02d2c97b66ea8dca
Alice Shen <ays0110@gmail.com>
2017-09-13 14:18:35 -0400

    New deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 git pull origin master
-cp custom-openseadragon-annotations.js node_modules/openseadragon-annotations/dist/openseadragon-annotations.js
 RAILS_ENV=production rake db:migrate
 RAILS_ENV=production rake assets:precompile
-touch tmp/restart.txt
+sudo /usr/local/bin/restartapache
 bundle exec sidekiq -d -L log/sidekiq.log -C config/sidekiq.yml -e production
