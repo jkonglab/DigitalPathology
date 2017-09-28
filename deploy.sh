ed1144e9982c9ac1a7b67c34026d813dea545ec6
Alice Shen <ays0110@gmail.com>
2017-09-28 15:35:35 -0400

    Add bundle install to deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 git pull origin master
+bundle install
 RAILS_ENV=production rake db:migrate
 RAILS_ENV=production rake assets:precompile
 sudo /usr/local/bin/restartapache
--- deploy_ian.sh
+++ deploy_ian.sh
@@ -1,3 +1,4 @@
 git pull origin master
+bundle install
 bundle exec sidekiqctl stop tmp/sidekiq.pid
 bundle exec sidekiq -d -L log/sidekiq.log -C config/sidekiq.yml -e production
