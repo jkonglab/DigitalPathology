81be05bd334a8dccf838014caeded9c919dad758
Alice Shen <ays0110@gmail.com>
2017-11-10 01:24:47 -0500

    Add Node Modules

--- deploy_ian.sh
+++ deploy_ian.sh
@@ -1,4 +1,5 @@
 git pull origin master
 bundle install
+RAILS_ENV=production rake db:migrate
 bundle exec sidekiqctl stop tmp/sidekiq.pid
 bundle exec sidekiq -d -L log/sidekiq.log -C config/sidekiq.yml -e production
