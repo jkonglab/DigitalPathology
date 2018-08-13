3b30a8cc16aa0809972c14b1cf7c0be920c9ffdb
Alice Shen <ays0110@gmail.com>
2018-08-13 14:41:08 -0700

    Add Steatosis Neural Net Scripts

---
+++ algorithms/python/steatosis_neural_net.py
@@ -0,0 +1,32 @@
+import numpy as np
+import theano
+import theano.tensor as T
+import pickle
+import lasagne
+import scipy.ndimage.filters as filters
+import matplotlib.pyplot as plt
+import skimage.data
+import os
+import sys
+import json
+
+def preprocess(raw_input, output_file_path, parameters):
+	return raw_input
+
+def postprocess(main_output, output_file_path):
+	import numpy as np
+	import json
+
+	output = json.dumps(np.asarray(main_output).T.tolist())
+
+	with open(output_file_path, 'w') as outfile:
+  		outfile.write(output)
+	return True
+
+def main(input, parameters):
+	sys.path.insert(0, './steatosis_neural_net')
+	algorithm_module = __import__('prediction')
+	function_handler = getattr(algorithm_module, 'Predict')
+	results = function_handler(input)
+	return results
+
