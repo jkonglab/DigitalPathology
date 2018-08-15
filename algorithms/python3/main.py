1d6198be37c65e25bb905fbdd42274e00f64b9cb
Alice Shen <ays0110@gmail.com>
2018-08-15 12:48:06 -0700

    Python3

---
+++ algorithms/python3/main.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import math
+import matplotlib
+matplotlib.use('Agg')
+from skimage import io
+
+def main(argv):
+	tile_folder_path = argv[0]
+	output_file_path = argv[1]
+	parameters = argv[2]
+	algorithm_name = argv[3]
+	tile_x = int(argv[4])
+	tile_y = int(argv[5])
+	tile_width = int(argv[6])
+	tile_height = int(argv[7])
+
+	# Get the roi region to be analyzed
+	dzi_size = 2000
+	dzi_x_index = int(math.floor(tile_x / dzi_size))
+	dzi_y_index = int(math.floor(tile_y / dzi_size))
+	x_offset = tile_x%dzi_size
+	y_offset = tile_y%dzi_size
+	image_file = get_dzi_file(tile_folder_path, dzi_x_index, dzi_y_index)
+	image = io.imread(image_file)
+	roi = image[y_offset:y_offset+tile_height,x_offset:x_offset+tile_width]	
+
+	# Set up function handlers to call correct algorithm
+	algorithm_module = __import__(algorithm_name)
+	preprocessing_function_handler = getattr(algorithm_module, 'preprocess')
+	main_function_handler = getattr(algorithm_module, 'main')
+	postprocessing_function_handler = getattr(algorithm_module, 'postprocess')
+
+	main_input = preprocessing_function_handler(roi, output_file_path, parameters)
+	main_output = main_function_handler(main_input, parameters)
+	postprocessing_function_handler(main_output, output_file_path)
+
+def get_dzi_file(tile_folder_path, dzi_x_index, dzi_y_index):
+	items = os.listdir(tile_folder_path)
+	folder_numbers = list(map(graceful_str2int, items))
+	maximum = max(folder_numbers);
+	image_file = tile_folder_path + '/' + str(maximum) + '/' + str(dzi_x_index) + '_' + str(dzi_y_index) + '.jpeg'
+	return image_file
+
+def graceful_str2int(string):
+	try:
+		return int(string)
+	except ValueError:
+		return 0
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
---
+++ algorithms/python3/steatosis_neural_net/Filter_prediction_results.py
@@ -0,0 +1,86 @@
+"""
+Mask R-CNN
+Train on the nuclei segmentation dataset from the
+Kaggle 2018 Data Science Bowl
+https://www.kaggle.com/c/data-science-bowl-2018/
+
+Licensed under the MIT License (see LICENSE for details)
+Written by Waleed Abdulla
+
+------------------------------------------------------------
+
+Usage: import the module (see Jupyter notebooks for examples), or run from
+       the command line as such:
+
+    # Train a new model starting from ImageNet weights
+    python3 steatosis.py train --dataset=/path/to/dataset --subset=train --weights=imagenet
+
+    # Train a new model starting from specific weights file
+    python3 steatosis.py train --dataset=/path/to/dataset --subset=train --weights=/path/to/weights.h5
+
+    # Resume training a model that you had trained earlier
+    python3 steatosis.py train --dataset=/path/to/dataset --subset=train --weights=last
+
+    # Generate submission file
+    python3 steatosis.py detect --dataset=/path/to/dataset --subset=train --weights=<last or /path/to/weights.h5>
+"""
+
+# Set matplotlib backend
+# This has to be done before other importa that might
+# set it, but only if we're running in script mode
+# rather than being imported.
+
+import os
+import sys
+import numpy as np
+import cv2
+import colorsys 
+import matplotlib
+from skimage import measure
+
+from steatosis2_Predict import random_colors
+
+def filer(min_score, max_score,r,image):
+    #pred_DIR = os.path.join(DATASET_DIR, "prediction_results/") 
+    #directory = os.path.dirname(pred_DIR)
+    #if not os.path.exists(directory):
+    #    os.makedirs(directory)
+    #image = cv2.imread(DATASET_DIR+"/"+image_name,1)  
+    #print(type(image))
+    result = r
+    scores = result['scores']
+    masks = result['masks']
+    order = np.argsort(scores)[::-1] + 1  # 1-based descending
+    masks = np.max(masks * np.reshape(order, [1, 1, -1]), -1)
+    alpha = 0.7
+    inx = -1
+    invalid = 0
+    N = len(result['class_ids'])
+    #print("class id N is {}".format(N))
+    colors = random_colors(N)
+    image = np.array(image)
+    image_masked = image
+    #print("image shape is {}".format(image.shape))
+    
+    for c_id in result['class_ids']:
+        inx = inx + 1
+        score = result['scores'][inx]
+        pred_mask = result['masks'][:, :, inx]
+        pred_mask = np.array(pred_mask, dtype=np.uint8)
+        #pred_mask = cv2.resize(pred_mask, ( 1024 , 1024 ))
+        color = colors[inx]
+        for c in range(3):
+            image_masked[:, :, c] = np.where(pred_mask == 1,image[:, :, c] *(1 - alpha) + alpha * color[c] * 255,image[:, :, c])
+        
+    return np.array(image_masked)
+    #cv2.imwrite(pred_DIR + '{}_mask.png'.format(image_name[:-4]), image_masked)
+    print("Finished!\n")
+
+def compute_features(mask_array):
+    from skimage.measure import label, regionprops
+    label_img = label(image)
+    regions = regionprops(label_img)
+
+
+
+   
