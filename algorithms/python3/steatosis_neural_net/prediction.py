f48a4e05ff450dd2d23931e703e5506b85bf1630
Alice Shen <ays0110@gmail.com>
2018-08-24 14:23:15 -0700

    Move random colors

--- algorithms/python3/steatosis_neural_net/prediction.py
+++ algorithms/python3/steatosis_neural_net/prediction.py
@@ -97,21 +97,6 @@ def Predict(image):
     results= r[0]
     print("In steatosis predict, r type is {}".format(type(results)))
     return filer(min_score, max_score,results,image)
-    
-        
-def random_colors(N, bright=True):
-    import colorsys
-    import random
-    """
-    Generate random colors.
-    To get visually distinct colors, generate them in HSV space then
-    convert to RGB.
-    """
-    brightness = 1.0 if bright else 0.7
-    hsv = [(i / N, 1, brightness) for i in range(N)]
-    colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
-    random.shuffle(colors)
-    return colors
 
 def get_ax(rows=1, cols=1, size=16):
     """Return a Matplotlib Axes array to be used in
