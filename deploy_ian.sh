c099304ce868e015cd0b070715429269d4596157
Alice Shen <ays0110@gmail.com>
2018-10-19 12:46:28 -0400

    Update ian script

--- deploy_ian.sh
+++ deploy_ian.sh
@@ -1,4 +1,6 @@
 git pull origin master
 bundle install --without development test
+RAILS_ENV=production rake db:migrate
+RAILS_ENV=production rake assets:precompile
 bundle exec sidekiqctl stop tmp/sidekiq.pid
 bundle exec sidekiq -d -L log/sidekiq.log -C config/sidekiq.yml -e production
