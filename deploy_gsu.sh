f432bd92af214e826bee9c22b9ea14e710a113fe
Alice Shen <ays0110@gmail.com>
2018-11-05 15:07:29 -0800

    Drop priviledge

--- deploy_gsu.sh
+++ deploy_gsu.sh
@@ -3,6 +3,6 @@ bundle install --without development test
 RAILS_ENV=production rake db:migrate
 RAILS_ENV=production rake assets:precompile
 rvmsudo bundle exec passenger stop
-rvmsudo bundle exec passenger start
+rvmsudo bundle exec passenger start --user ashen2
 bundle exec sidekiqctl stop tmp/sidekiq.pid
 bundle exec sidekiq -d -L log/sidekiq.log -C config/sidekiq.yml -e production 
