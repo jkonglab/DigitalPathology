90a7c9526688105a9f1a8d2afc023050aa0d2f06
Alice Shen <ays0110@gmail.com>
2018-11-04 18:26:27 -0800

    Add extra flags

--- deploy_gsu.sh
+++ deploy_gsu.sh
@@ -2,6 +2,6 @@ git pull origin master
 bundle install --without development test
 RAILS_ENV=production rake db:migrate
 RAILS_ENV=production rake assets:precompile
-sudo /usr/local/rvm/gems/ruby-2.5.1/wrappers/bundle exec passenger-config restart
+sudo /usr/local/rvm/gems/ruby-2.5.1/wrappers/bundle exec passenger-config restart-app . --ignore-app-not-running --ignore-passenger-not-running
 bundle exec sidekiqctl stop tmp/sidekiq.pid
 bundle exec sidekiq -d -L log/sidekiq.log -C config/sidekiq.yml -e production
