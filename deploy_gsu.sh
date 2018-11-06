d44af121db82ff8703210792ae86cb037f04e6f2
Alice Shen <ays0110@gmail.com>
2018-11-05 16:24:28 -0800

    Nope

--- deploy_gsu.sh
+++ deploy_gsu.sh
@@ -4,5 +4,5 @@ RAILS_ENV=production rake db:migrate
 RAILS_ENV=production rake assets:precompile
 rvmsudo bundle exec passenger stop
 rvmsudo bundle exec passenger start
-bundle exec sidekiqctl stop tmp/sidekiq.pid
-bundle exec sidekiq -d -L log/sidekiq.log -C config/sidekiq.yml -e production 
+rvmsudo bundle exec sidekiqctl stop tmp/sidekiq.pid
+rvmsudo bundle exec sidekiq -d -L log/sidekiq.log -C config/sidekiq.yml -e production 
