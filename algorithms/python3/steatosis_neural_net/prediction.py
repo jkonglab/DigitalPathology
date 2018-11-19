2d5f7613a528c123057adc8911d82627e9befea4
Alice Shen <ays0110@gmail.com>
2018-11-19 02:54:31 -0800

    Update algorithms/python3/steatosis_neural_net/prediction.py

--- algorithms/python3/steatosis_neural_net/prediction.py
+++ algorithms/python3/steatosis_neural_net/prediction.py
@@ -11,6 +11,7 @@ import time
 import numpy as np
 import tensorflow as tf
 from mrcnn import utils
+from mrcnn.config import Config
 import mrcnn.model as modellib
 import numpy as np
 import cv2
@@ -54,6 +55,72 @@ def load_image(config,image):
                                     window, scale, p.zeros([self.config.NUM_CLASSES], dtype=np.int32))
 
     return image, image_meta
+
+
+class SteatosisConfig(Config):
+    """Configuration for training on the steatosis segmentation dataset."""
+    # Give the configuration a recognizable name
+    NAME = "steatosis"
+
+    # Adjust depending on your GPU memory
+    IMAGES_PER_GPU = 1
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # Background + steatosis
+
+    # Number of training and validation steps per epoch
+    STEPS_PER_EPOCH = (161 - 20) // IMAGES_PER_GPU
+    VALIDATION_STEPS = max(1, 20 // IMAGES_PER_GPU)
+
+    # Don't exclude based on confidence. Since we have two classes
+    # then 0.5 is the minimum anyway as it picks between steatosis and BG
+    DETECTION_MIN_CONFIDENCE = 0
+
+    # Backbone network architecture
+    # Supported values are: resnet50, resnet101
+    BACKBONE = "resnet50"
+
+    # Input image resizing
+    # Random crops of size 512x512
+    IMAGE_RESIZE_MODE = "crop"
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+    IMAGE_MIN_SCALE = 2.0
+
+    # Length of square anchor side in pixels
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
+
+    # ROIs kept after non-maximum supression (training and inference)
+    POST_NMS_ROIS_TRAINING = 1000
+    POST_NMS_ROIS_INFERENCE = 2000
+
+    # Non-max suppression threshold to filter RPN proposals.
+    # You can increase this during training to generate more propsals.
+    RPN_NMS_THRESHOLD = 0.9
+
+    # How many anchors per image to use for RPN training
+    RPN_TRAIN_ANCHORS_PER_IMAGE = 64
+
+    # Image mean (RGB)
+    MEAN_PIXEL = np.array([43.53, 39.56, 48.22])
+
+    # If enabled, resizes instance masks to a smaller size to reduce
+    # memory load. Recommended when using high-resolution images.
+    USE_MINI_MASK = True
+    MINI_MASK_SHAPE = (56, 56)  # (height, width) of the mini-mask
+
+    # Number of ROIs per image to feed to classifier/mask heads
+    # The Mask RCNN paper uses 512 but often the RPN doesn't generate
+    # enough positive proposals to fill this and keep a positive:negative
+    # ratio of 1:3. You can increase the number of proposals by adjusting
+    # the RPN NMS threshold.
+    TRAIN_ROIS_PER_IMAGE = 128
+
+    # Maximum number of ground truth instances to use in one image
+    MAX_GT_INSTANCES = 200
+
+    # Max number of final detections per image
+    DETECTION_MAX_INSTANCES = 400
 
 class SteatosisInferenceConfig(SteatosisConfig):
     # Set batch size to 1 to run one image at a time
