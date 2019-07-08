0dbd65c80815b4c45e068fdb4f8de2c1596cfaf6
Alice Shen <ays0110@gmail.com>
2019-07-08 01:56:32 -0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.action_controller.default_url_options = {:host=>ENV['ROOT_URL']}
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
