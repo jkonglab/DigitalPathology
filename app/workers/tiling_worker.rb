034c003b38dbb6ce586187b277d49a77fca08de5
Divya <divya.bhuvanapalli@gmail.com>
2019-11-01 15:16:17 -0400

    tilesprocessed fix

--- app/workers/tiling_worker.rb
+++ app/workers/tiling_worker.rb
@@ -46,12 +46,18 @@ class TilingWorker
 
             tiles_file = File.read(File.join(@run.run_folder, '/tiles_to_analyze.json'))
             tiles = JSON.parse(tiles_file)
-	    num_tiles_counter = tiles.count
+	    tiles.each do |tile|
+                tile_x = tile.split(',')[0].to_i
+                tile_y = tile.split(',')[1].to_i
+                if tile_x != @image.width and tile_y != @image.height
+                        num_tiles_counter += 1
+		end
+	    end
+	    @run.update_attributes!(:total_tiles=>num_tiles_counter)
             tiles.each do |tile|
                 tile_x = tile.split(',')[0].to_i
                 tile_y = tile.split(',')[1].to_i
-		if tile_x != @image.width and tile_y != @image.height
-			#num_tiles_counter += 1
+		if tile_x != @image.width and tile_y != @image.height 
 		        if @algorithm.single_queue_flag
         	          Sidekiq::Client.push('queue' => 'single_analysis_queue', 'class' =>  AnalysisWorker, 'args' => [run_id, user_id,tile_x, tile_y])
                 	else
@@ -60,7 +66,6 @@ class TilingWorker
 		end
             end
         end
-        @run.update_attributes!(:total_tiles=>num_tiles_counter)
         @run.check_if_done
     end
   end
