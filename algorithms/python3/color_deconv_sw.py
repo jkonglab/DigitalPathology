b49b5d72f46d401ea687326088ec46b8c80d9aef
Divya Bhuvanapalli <divya.bhuvanapalli@gmail.com>
2019-12-19 18:05:33 -0500

    algorithm fixes

--- algorithms/python3/color_deconv_sw.py
+++ algorithms/python3/color_deconv_sw.py
@@ -10,8 +10,8 @@ def postprocess(main_output, output_file_path):
 	sys.path.insert(0, './color_deconv_utils')
 	algorithm_module = __import__('color_deconv_utils')
 	function_handler = getattr(algorithm_module,'save_im')
-	mask, name = main_output
-	function_handler(os.path.join(output_file_path, name+'_white_pixel_mask.png'), mask)
+	mask = main_output
+	function_handler(os.path.join(output_file_path, 'output_white_pixel_mask.png'), mask)
 
 
 def main(img, params):
@@ -19,13 +19,8 @@ def main(img, params):
 	sys.path.insert(0, './color_deconv_utils')
 	algorithm_module = __import__('color_deconv_utils')
 	function_handler = getattr(algorithm_module,'hsv_get_white')
-	if params[0] == 'default':
-		tc = localtime(time())
-		name = '{:04d}{:02d}{:02d}{:02d}{:02d}{:02d}'.format(tc.tm_year, tc.tm_mon, tc.tm_mday, tc.tm_hour, tc.tm_min, tc.tm_sec)
-	else:
-		name = params[0]
 	mask = function_handler(img)
-	return mask, name
+	return mask
 
 
 
--- algorithms/python3/fibrosis/predict.py
+++ algorithms/python3/fibrosis/predict.py
@@ -5,7 +5,7 @@ import numpy as np
 import random
 import os
 
-def predict_return(X):
+def predict_return2(X):
 # This prediction funtion return prediction 
 	n_classes = 2
 	model_name = 'fcn8'
@@ -38,3 +38,45 @@ def predict_return(X):
 			if(seg_img[i,j]):
 				result.append([i,j])
 	return result
+
+
+def predict_return(X):
+# This prediction funtion return target contours and the number of target pixels
+	n_classes = 2
+	model_name = 'fcn8'
+
+	input_width =  256
+	input_height = 256
+	epoch_number = 1
+
+	modelFns = { 'vgg_segnet':Models.VGGSegnet.VGGSegnet , 'vgg_unet':Models.VGGUnet.VGGUnet , 'vgg_unet2':Models.VGGUnet.VGGUnet2 , 'fcn8':Models.FCN8.FCN8 , 'fcn32':Models.FCN32.FCN32   }
+	modelFN = modelFns[ model_name ]
+	save_weights_path = '/DP_Share/imageviewer/algorithms/python3/fibrosis/image-segmentation-keras-master.model.9'
+	m = modelFN( n_classes , input_height=input_height, input_width=input_width   )
+	m.load_weights(  save_weights_path  )
+	m.compile(loss='categorical_crossentropy',
+      	optimizer= 'adadelta' ,
+      	metrics=['accuracy'])
+
+	output_height = 256
+	output_width = 256
+	colors = [(0,0,0),(255,255,255)]
+	pr = m.predict( np.array([X]) )[0]
+	pr = pr.reshape(( output_height ,  output_width , n_classes ) ).argmax( axis=2 )
+
+	# target pixel number
+	tp_num = np.sum(pr)
+	
+	# target contours
+	cts, _ = cv2.findContours(pr.astype(dtype=np.uint8), cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+	contours = []
+	for ct in cts:
+		if len(ct) < 6:
+			continue
+		contours.append(ct.squeeze().tolist())
+
+	return contours, tp_num
+
+
+
+
