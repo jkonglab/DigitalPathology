b318742bc214316d28ff9708e794f48805b4c2e5
Divya Bhuvanapalli <divya.bhuvanapalli@gmail.com>
2020-04-06 09:17:40 -0400

    node modules uninstalled with annotations

--- node_modules/openseadragon-annotations/src/actions/initialize.js
+++
@@ -1,6 +0,0 @@
-export default function initialize(options, Dispatcher) {
-  Dispatcher.dispatch({
-    type: 'INITIALIZE',
-    options,
-  });
-}
--- node_modules/openseadragon-annotations/src/actions/zoom.js
+++
@@ -1,6 +0,0 @@
-export default function zoom(zoomLevel, Dispatcher) {
-  Dispatcher.dispatch({
-    type: 'ZOOM_UPDATE',
-    zoom: zoomLevel,
-  });
-}
